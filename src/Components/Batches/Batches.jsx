0c93f922a462673193e89f7f69b3ee9d2201fd3a
Dhanashree-02 <dhanashree.sonune02@gmail.com>
2024-09-12 16:41:03 -1000

    Batches

--- src/Components/Batches/Batches.jsx
+++ src/Components/Batches/Batches.jsx
@@ -1,9 +1,30 @@
+import Hero from "../Home/Hero/Hero";
+import UpcomingBatches from "../Home/UpcomingBatches/UpcomingBatches";
+import WhyJoin from "../Home/WhyJoin/WhyJoin";
+import styles from "./Batches.module.css"; // Importing the CSS module
+
+
 
 function Batches() {
   return (
     <div>
-       <h1>hello</h1>
-       sakjdnskdfsdf
+        <Hero />
+        <UpcomingBatches />
+      {/* Terms and Conditions Section */}
+      <section className={styles.termsSection}>
+        <h2>Please read the Terms and Conditions (T&Cs) carefully before enrolling in the course</h2>
+        <ul>
+          <li>The fees will be non-refundable and non-transferable in any circumstances.</li>
+          <li>Batch or course change is strictly not allowed.</li>
+          <li>Nexus provides corporate-level training.</li>
+          <li>Training will be provided according to the specified Syllabus.</li>
+          <li>Candidates will receive interview calls from different job portals.</li>
+        </ul> <br />
+        <p>All rights reserved to Nexus - Corporate Training Center*</p><br />
+        <p>If you have any queries/doubt regarding payment, please contact us at contact@Nexus-CTC-LLP</p>
+      </section>
+      <WhyJoin />
+
     </div>
   )
 }
