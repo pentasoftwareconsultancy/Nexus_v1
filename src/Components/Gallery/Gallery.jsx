a8b3372a7d6db6fe237c05a2f61187bec9d04c6e
Dhanashree-02 <dhanashree.sonune02@gmail.com>
2024-10-16 16:57:35 -1000

    Gallery Page

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React from 'react';
+import Slider from './Slider/Slider';
+import HeroSlider from './HeroSlider/HeroSlider';
 import styles from './Gallery.module.css';
+
+const images = [
+  
+  { src: 'https://i.ibb.co/Bq4Q0M8/img4.jpg',  },
+  { src: 'https://i.ibb.co/jTQfmTq/img5.jpg',  },
+  { src: 'https://i.ibb.co/RNkk6L0/img6.jpg',  },
+];
+
+const images1 = [
+  { src: 'https://i.ibb.co/qCkd9jS/img1.jpg', },
+  { src: 'https://i.ibb.co/jrRb11q/img2.jpg', },
+  { src: 'https://i.ibb.co/NSwVv8D/img3.jpg',  },
+  
+];
 
 function Gallery() {
   return (
+
     <div>
-      <div className={styles.container}>
-        <h1>Hello </h1>
+        <HeroSlider />
+      <Slider />
+
+
+    <div className={styles.galleryContainer}>
+
+      <h1 className={styles.title}>Seminar Photos </h1>
+      <div className={styles.galleryGrid}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.galleryItem}>
+            <img src={image.src} alt={image.title} className={styles.galleryImage} />
+            {/* <div className={styles.imageTitle}>{image.title}</div> */}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.galleryGrid1}>
+        {images1.map((image, index) => (
+          <div key={index} className={styles.galleryItem}>
+            <img src={image.src} alt={image.title} className={styles.galleryImage} />
+            {/* <div className={styles.imageTitle}>{image.title}</div> */}
+          </div>
+        ))}
+      </div>
+
       </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
---
+++ src/Components/Gallery/HeroSlider/HeroSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './HeroSlider.module.css'
+
+function HeroSlider() {
+  return (
+    <div className={styles.HeroSlider}>
+      <h1>Hero Slider </h1>
+    </div>
+  )
+}
+
+export default HeroSlider
