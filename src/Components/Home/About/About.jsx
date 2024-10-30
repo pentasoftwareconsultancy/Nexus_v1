5d2ed86c95c4c7285b3e965daa6fcc21e03c608e
Saurabh <nirmalesaurabh99@gmail.com>
2024-10-30 12:23:21 +0530

    about us add in home page

---
+++ src/Components/Home/About/About.jsx
@@ -0,0 +1,27 @@
+import styles from "./About.module.css";
+
+function About() {
+  return (
+    <div>
+      <section className={styles.aboutUs}>
+        <div>
+          <h1 className={styles.headline}>About Us</h1>
+        </div>
+        <p>
+          Nexus Corporate Training Center LLP is Pune’s most innovative IT
+          training institute, offering a wide range of specialized courses like
+          Medical Coding, Power BI, AWS, and other software courses with
+          placement assistance. Our mission is to provide learners with
+          industry-relevant skills through hands-on training, led by experienced
+          instructors. We focus on delivering the best software courses in Pune
+          with placement, ensuring our students are job-ready upon course
+          completion. With a strong emphasis on practical knowledge, Nexus
+          equips learners to excel in the competitive IT landscape and secure
+          promising careers in top companies.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default About;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,26 +1,20 @@
 import { Helmet } from "react-helmet";
-import Course from '../../Components/Home/Course/Course';
-import Home from '../../Components/Home/Hero/Hero';
-import UpcomingBatches from '../../Components/Home/UpcomingBatches/UpcomingBatches';
-import WhyJoin from '../../Components/Home/WhyJoin/WhyJoin';
-import FadeInSection from '../../Hookes/useInView';
-import PartnerPage from '../Partner/PartnerPage';
+import Course from "../../Components/Home/Course/Course";
+import Home from "../../Components/Home/Hero/Hero";
+import UpcomingBatches from "../../Components/Home/UpcomingBatches/UpcomingBatches";
+import WhyJoin from "../../Components/Home/WhyJoin/WhyJoin";
+import FadeInSection from "../../Hookes/useInView";
+import PartnerPage from "../Partner/PartnerPage";
 import Review from "../../Components/Review/Review";
-
+import About from "../../Components/Home/About/About";
 
 function HomePage() {
   return (
     <div>
       <Helmet>
         <title>Nexus Corporate Training Center - Home </title>
-        <meta
-          name="description"
-          content="Know more about Nexus "
-        />
-        <meta
-          name="keywords"
-          content="Nexus"
-        />
+        <meta name="description" content="Know more about Nexus " />
+        <meta name="keywords" content="Nexus" />
       </Helmet>
 
       <FadeInSection>
@@ -38,7 +32,7 @@ function HomePage() {
       </FadeInSection>
       <FadeInSection>
         {/* <UpcomingEvents /> */}
-
+        <About />
         <Review />
       </FadeInSection>
     </div>
